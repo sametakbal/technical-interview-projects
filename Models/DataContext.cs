c656c53200a82051720d82d50a687dc218731204
Samet Akbal <sametakbal@github.com>
2020-12-15 22:20:22 +0300

    Update Context

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -1,9 +1,12 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace weather_app.Models
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<User> User{get;set;}
     }
 }
