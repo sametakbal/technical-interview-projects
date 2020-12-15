e2b53fbb1ae858d9d8dfe0cace71045c4b652560
Samet Akbal <sametakbal@github.com>
2020-12-15 22:20:02 +0300

    Migrations added and database update

---
+++ Migrations/20201215184022_first.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace weather_app.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    IsAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
