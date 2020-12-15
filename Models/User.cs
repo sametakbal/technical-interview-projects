b4910b757172d897de94a4f9039c6ed83168f1c2
Samet Akbal <sametakbal@github.com>
2020-12-15 21:25:55 +0300

    User Model created

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace weather_app.Models
 {
     public class User
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(55)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(55)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(55)]
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }=false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastLogin { get; set; }
     }
 }
