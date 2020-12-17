2a21e8b9e152c67beba0f05e0a19053f69847522
Samet Akbal <sametakbal@github.com>
2020-12-17 11:07:47 +0300

    Update Report Attributes

--- Models/Report.cs
+++ Models/Report.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace weather_app.Models
 {
@@ -7,9 +8,14 @@ namespace weather_app.Models
     {
         [Key]
         public int Id { get; set; }
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        public User User { get; set; }
         public DateTime QueryTime { get; set; }
+        [ForeignKey("Location")]
         public int LocationId { get; set; }
+        public Location Location { get; set; }
+        [StringLength(45)]
         public string IpAddress { get; set; }
         public string QueryResult { get; set; }
         public double QueryFetchTime { get; set; }
