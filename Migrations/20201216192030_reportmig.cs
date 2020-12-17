830b24a99ddff5d4b413361f0192ffd9f889fc71
Samet Akbal <sametakbal@github.com>
2020-12-17 11:05:42 +0300

    Database update

---
+++ Migrations/20201216192030_reportmig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace weather_app.Migrations
+{
+    public partial class reportmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    QueryTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(45)", maxLength: 45, nullable: true),
+                    QueryResult = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QueryFetchTime = table.Column<double>(type: "float", nullable: false),
+                    isSuccess = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Report_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_LocationId",
+                table: "Report",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_UserId",
+                table: "Report",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -5,9 +5,10 @@ namespace weather_app.Models
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<User> User{get;set;}
-        public DbSet<Location> Location{get;set;}
+        public DbSet<User> User { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Report> Report { get; set; }
     }
 }
