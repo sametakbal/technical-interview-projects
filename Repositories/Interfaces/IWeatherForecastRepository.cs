303afd715f52c929e1a93e2f2846e7aa58590dee
Samet Akbal <sametakbal@github.com>
2020-12-17 11:10:51 +0300

    Create IWeatherForecastRepository.cs

---
+++ Repositories/Interfaces/IWeatherForecastRepository.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using weather_app.Models;
+using weather_app.Models.Dtos;
+
+namespace weather_app.Repositories.Interfaces
+{
+    public interface IWeatherForecastRepository
+    {
+         Task<WeatherForecast> GetWeatherForecast(WFRequestDto wFRequest);
+    }
+}
