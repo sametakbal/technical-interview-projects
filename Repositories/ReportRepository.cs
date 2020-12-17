42e7f5f212dd3c92bfcc201722cd79a46bcb8832
Samet Akbal <sametakbal@github.com>
2020-12-17 18:48:01 +0300

    Update ReportRepository.cs

--- Repositories/ReportRepository.cs
+++ Repositories/ReportRepository.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using weather_app.Models;
+using weather_app.Models.Dtos;
 using weather_app.Repositories.Interfaces;
 
 namespace weather_app.Repositories
@@ -11,6 +15,18 @@ namespace weather_app.Repositories
         public ReportRepository(DataContext context)
         {
             _context = context;
+        }
+
+        public async Task<ReportPageDto> GetReports(ReportPageDto reportPageDto)
+        {
+            reportPageDto.Reports = await _context.Report.Where(r => r.UserId == reportPageDto.UserId &&
+            DateTime.Compare(reportPageDto.StartDate, r.QueryTime) < 0 &&
+            DateTime.Compare(reportPageDto.EndDate, r.QueryTime) > 0 &&
+            reportPageDto.QueryState
+            ).OrderByDescending(report => report.QueryTime).Include(rep => rep.Location).ToListAsync();
+            reportPageDto.Users = await _context.User.ToListAsync();
+            
+            return reportPageDto;
         }
 
         public async Task<bool> SaveReport(Report report)
