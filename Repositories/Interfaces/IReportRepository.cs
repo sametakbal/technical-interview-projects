dd25ccd18d5bca68f5a7d4e75d2d3e048d4b62b5
Samet Akbal <sametakbal@github.com>
2020-12-17 18:47:57 +0300

    Update IReportRepository.cs

--- Repositories/Interfaces/IReportRepository.cs
+++ Repositories/Interfaces/IReportRepository.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using weather_app.Models;
+using weather_app.Models.Dtos;
 
 namespace weather_app.Repositories.Interfaces
 {
     public interface IReportRepository
     {
-         Task<bool> SaveReport(Report report);
+        Task<bool> SaveReport(Report report);
+        Task<ReportPageDto> GetReports(ReportPageDto reportPageDto);
     }
 }
